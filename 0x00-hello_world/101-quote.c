da2a8cdbe3a764ea2e6d70423423a969ccc401cb
Right Praise <rightpraise@gmail.com>
2022-09-08 18:03:15 -0700

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,74 +1,12 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - prints the standard error sandwiched around the standard output
  *
- * Return: Always 0 (Success)
+ * Return: returns 1
  */
 
 int main(void)
 {
-	putchar ('a');
-	putchar ('n');
-	putchar ('d');
-	putchar (' ');
-	putchar ('t');
-	putchar ('h');
-	putchar ('a');
-	putchar ('t');
-	putchar (' ');
-	putchar ('p');
-	putchar ('i');
-	putchar ('e');
-	putchar ('c');
-	putchar ('e');
-	putchar (' ');
-	putchar ('o');
-	putchar ('f');
-	putchar (' ');
-	putchar ('a');
-	putchar ('r');
-	putchar ('t');
-	putchar (' ');
-	putchar ('i');
-	putchar ('s');
-	putchar (' ');
-	putchar ('u');
-	putchar ('s');
-	putchar ('e');
-	putchar ('f');
-	putchar ('u');
-	putchar ('l');
-	putchar ('"');
-	putchar (' ');
-	putchar ('-');
-	putchar (' ');
-	putchar ('D');
-	putchar ('o');
-	putchar ('r');
-	putchar ('a');
-	putchar (' ');
-	putchar ('K');
-	putchar ('o');
-	putchar ('r');
-	putchar ('p');
-	putchar ('a');
-	putchar ('r');
-	putchar (',');
-	putchar (' ');
-	putchar ('2');
-	putchar ('0');
-	putchar ('1');
-	putchar ('5');
-	putchar ('-');
-	putchar ('1');
-	putchar ('0');
-	putchar ('-');
-	putchar ('1');
-	putchar ('9');
-	putchar ('\n');
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
-
