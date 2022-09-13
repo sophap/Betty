1c4f69f3405709ebc4a005743b98bfbac8e04644
Right Praise <rightpraise@gmail.com>
2022-09-13 10:45:33 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	int d = 'a';
 
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * main - prints aphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+
