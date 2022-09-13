21934affb443042327019d0dec1116b2c769ed89
Right Praise <rightpraise@gmail.com>
2022-09-13 12:36:37 -0700

    Done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * _islower - Entry point
@@ -9,7 +8,9 @@
 int _islower(int c)
 {
 	int r;
+
 	r = _islower(c);
 	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
