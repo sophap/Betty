8f376400f24af3e0f4910bd521726c2e801d48b3
Right Praise <rightpraise@gmail.com>
2022-09-13 14:31:19 -0700

    Done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,13 +13,13 @@ int _islower(int c)
 
 	for (r = 'a'; r <= 'z'; r++)
 	{
-	if (c == r)
+	if (c == r++)
 	{
-	return ('1');
+	return (1);
 	}
 	else
 	{
-	return ('0');
+	return (0);
 	}
 	}
 	_putchar('\n');
