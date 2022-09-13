1c071b820c584aaed049115e64f37e4fc6bd694a
Right Praise <rightpraise@gmail.com>
2022-09-13 15:21:29 -0700

    Done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,13 +13,13 @@ int print_sign(int n)
 	{
 		return (1);
 		_putchar('+');
-	}
+	};
 	else if (n == 0)
 	{
 		return (0);
 		_putchar('0');
-	}
-	else;
+	};
+	else
 	{
 		return (-1);
 		_putchar('-');
