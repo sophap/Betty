19da45b4b23b8bf147e3c797050d5bc0fef342ca
Right Praise <rightpraise@gmail.com>
2022-09-13 08:34:07 -0700

    Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar(char _);
+	_putchar(char p);
+	_putchar(char u);
+	_putchar(char t);
+	_putchar(char c);
+	_putchar(char h);
+	_putchar(char a);
+	_putchar(char r);
+	_putchar(char \n);
 
 return (0);
 }
