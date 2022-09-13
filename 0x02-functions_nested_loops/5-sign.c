a89c9a027eb5568192a3f131e79a33b906fc42df
Right Praise <rightpraise@gmail.com>
2022-09-13 15:13:56 -0700

    Done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main'h"
+
+/**
+ * print_sign -  prints the sign of a number
+ *
+ * @n: number to be checked by the function print_sign
+ *
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n = 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	return (0);
+}
