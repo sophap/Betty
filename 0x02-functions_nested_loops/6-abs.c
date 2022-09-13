ddb87f6d585ee6498a9648989d49a0e087e01a76
Right Praise <rightpraise@gmail.com>
2022-09-13 15:55:24 -0700

    Done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,4 +3,13 @@
 /**
  * _abs - computes absolute value of an integer
  *
+ * @i: integer to be checked by _abs
  *
+ * Return: Always 0
+ */
+int _abs(int i)
+{
+	return (i * (( i > 0) - (i < 0));
+}
+
+
