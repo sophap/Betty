3dc962a04c0f3369b703ad8191bf3db85cd6e114
Right Praise <rightpraise@gmail.com>
2022-09-14 16:40:03 -0700

    Removes file 2-largest_nuumber.c and adds it again

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns rhe largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -14,15 +14,16 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-	largest = a;
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
-	largest = b;
+		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
+
 	return (largest);
 }
