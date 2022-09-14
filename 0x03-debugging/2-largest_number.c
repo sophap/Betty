6062f5b84de8a0111195c5afec046abdb5701075
Right Praise <rightpraise@gmail.com>
2022-09-14 11:03:00 -0700

    Fixes code in 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,10 +12,10 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b > c)
+	if (a > b && b > c)
 	largest = a;
 
-	else if (b > a > c)
+	else if (b > a && a > c)
 	largest = b;
 	
 	else
