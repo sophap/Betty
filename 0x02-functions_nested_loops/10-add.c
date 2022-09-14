61da33e8e5da4f775d080e7e04809441329e50af
Right Praise <rightpraise@gmail.com>
2022-09-13 19:23:15 -0700

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds teo integers anr returns the result
+ *
+ * @a: the first interger to be added
+ * @b: the secod integer to be added
+ *
+ * Return: returns the result of the addition
+ */
+int add(int a, int b)
+{
+ 	int i;
+	i = a + b;
+
+	_putchar(i + '0');
+	return (i);
+}
