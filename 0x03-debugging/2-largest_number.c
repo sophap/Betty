55774778c4891bc1ba2f1750052bedcb67ebf70d
Right Praise <rightpraise@gmail.com>
2022-09-14 16:22:48 -0700

    Returns braces around the if statement and removes the int type on the input values of 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	a = (int)a;
-	b = (int)b;
-	c = (int)c;
-
 	if (a > b && a > c)
-
+	{
 	largest = a;
-
+	}
 	else if (b > a && b > c)
-
+	{
 	largest = b;
-
+	}
 	else
-
+	{
 	largest = c;
-
+	}
 	return (largest);
 }
