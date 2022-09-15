a40e5ffe8c1316816b6cc9ca1d5bcd8093de6b5a
Right Praise <rightpraise@gmail.com>
2022-09-15 14:19:57 -0700

    Removes trailing white spaces and adds description for main

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * prints the numbers from 1 to 100, followed by a new line.
+ * main - prints the numbers from 1 to 100, followed by a new line.
  * But for multiples of three print Fizz instead of the number
  * and for the multiples of five print Buzz.
  * For numbers which are multiples of both three and five print FizzBuzz.
+ *
  * Return: Always 0
  */
 int main(void)
@@ -23,7 +24,7 @@ int main(void)
 		else if (i % 3 == 0)
 		printf("Fizz ");
 
-		else 
+		else
 		printf("%d ", i);
 	}
 return (0);
