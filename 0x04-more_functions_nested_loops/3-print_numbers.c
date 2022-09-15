d1cb8075a9475d6ca28e74bc1f583d9bc672bf59
Right Praise <rightpraise@gmail.com>
2022-09-15 06:07:52 -0700

    Prints numbers 0 to 9 followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 followed by a newline
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int f = '0';
+
+	while (f <= '9')
+	{
+		_putchar(f + '0');
+		f++;
+	}
+	_putchar('\n');
+}
