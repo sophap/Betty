f36bf647bed2774d5ddc507ac33ccd03f5e47dcd
Right Praise <rightpraise@gmail.com>
2022-09-15 09:28:21 -0700

    Updates code

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,18 @@
 
 void print_diagonal(int n)
 {
-	if (n < 0 || n == 0)
+	int i;
+	int j;
+
+	if (n < '0' || n == '0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 1; i <= n; i++)
+		for (i = '1'; i <= n; i++)
 		{
-			for (int j = 1; j < i; j++)
+			for (j = '1'; j < i; j++)
 			{
 				_putchar(' ');
 			}
