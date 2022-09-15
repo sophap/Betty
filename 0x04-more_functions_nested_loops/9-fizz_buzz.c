01f20947603983edea11975129629b6a207162e6
Right Praise <rightpraise@gmail.com>
2022-09-15 14:13:42 -0700

    Replaces numbers divisible by 5 with Buzz, numbers divisible by 3 with Fizz and numbers divisible by 3 and 5 with FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * prints the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz instead of the number
+ * and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 100;
+	int i = 1;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 15 == 0)
+		printf("FizzBuzz ");
+
+		else if (i % 5 == 0)
+		printf("Buzz ");
+
+		else if (i % 3 == 0)
+		printf("Fizz ");
+
+		else 
+		printf("%d ", i);
+	}
+return (0);
+}
