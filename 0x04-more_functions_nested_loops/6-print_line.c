88358f9fba5e6fb42595fcf46808309d67e8188e
Right Praise <rightpraise@gmail.com>
2022-09-15 07:43:15 -0700

    Adds a for statement to the code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,18 @@
 
 void print_line(int n)
 {
-	int j = '_';
+	int j;
 
-	if (n < 0 || n == 0)
+	for (j = 0; j < n; j++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(j);
+		if (n < 0 || n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
