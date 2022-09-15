919805c4a2da61c22c948beac2fea94e84aff586
Right Praise <rightpraise@gmail.com>
2022-09-15 13:19:31 -0700

    Prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,5 +8,22 @@
 
 void print_square(int size)
 {
+	int m;
+	int n;
 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (m = 0; m < size; m++)
+		{
+			for (n = 0; n < size; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
