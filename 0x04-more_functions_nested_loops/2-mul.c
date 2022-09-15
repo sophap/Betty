2d9f59cafca53d9aaed2bd06b1f45f255ca3ada6
Right Praise <rightpraise@gmail.com>
2022-09-15 05:55:00 -0700

    Updates comment, removes trailing white spaces and adds blank line to code

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,14 @@
  * mul - multiplies two integers
  * @a: first integer to be multiplied
  * @b: second integer to be multiplied
- * @d: result of multiplied integers
  * Return: Always 0
  */
 
 int mul(int a, int b)
 {
 	int d;
+
 	d = a * b;
-	
+
 	return (d);
 }
