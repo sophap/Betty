e5c1e83b2f320b6d2fe568d78105be8c745c1504
Right Praise <rightpraise@gmail.com>
2022-09-16 05:53:36 -0700

    Prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the number to be printed
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	int t;
+	int v;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	t = 1;
+	v = 1;
+	while (c)
+	{
+		if (n / (t * 10) > 0)
+			t *= 10;
+		else
+		v = 0;
+	}
+	while (n >= 0)
+	{
+		if (t == 1)
+		{
+			_putchar(n % 10 + '0');
+			n = -1;
+		}
+		else
+		{
+			_putchar((n / t % 10) + '0');
+			t /= 10;
+		}
+	}   
+}
