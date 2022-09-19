93ee5425e607e99970ec5a69e6b3cea104f3dab3
Right Praise <rightpraise@gmail.com>
2022-09-19 10:03:01 -0700

    Ú

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @*s: pointer that checks the length of a string
+ * @s: pointer that checks the length of a string
  * Return: Always 0
  */
 int _strlen(char *s)
