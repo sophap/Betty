e345dd85ae569dc345130ef8fe5f1a951fe37622
Right Praise <rightpraise@gmail.com>
2022-09-19 09:26:41 -0700

    Swaps the value of 2 files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of 2 integers
+ * @a: first integer to be swapped
+ * @b: second integer to be swapped
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	c = *b;
+}
