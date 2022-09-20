f1568c0bdd69768afcf68e551ebb750186dfa272
Right Praise <rightpraise@gmail.com>
2022-09-19 19:16:21 -0700

    Updates code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,10 @@ void puts2(char *str)
 	int i;
 	int l = strlen(str);
 
-	for (i = 0; i < l / 2; i++)
+	for (i = 0; *t[i] != '\0'; i++)
 	{
-		printf("%c", *t);
-		t += 2;
+		if (i % 2 == 0)
+			printf("%c", *t[i]);
 	}
 	putchar('\n');
 }
