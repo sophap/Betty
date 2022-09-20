c0413d3bf1b52b0953fe1f24ba18193497609324
Right Praise <rightpraise@gmail.com>
2022-09-19 18:32:53 -0700

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to be reversed
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int l;
+	int i;
+	char *start;
+	char *end;
+	char temp;
+
+	l = strlen(s);
+	start = s;
+	end = s;
+
+	for (i = 0; i < l - 1; i++)
+		end++;
+
+	for (i = 0; i < l/2; i++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+
+		start++;
+		end--;
+	}
+}
