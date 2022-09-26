a109f296fb10b7ebf42feda8080b025f5a3b999f
Right Praise <rightpraise@gmail.com>
2022-09-26 14:50:41 -0700

    Adds to the for loop

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int a;
 	unsigned int b;
 
-	for (a = 0; dest[a] != '\0'; a++)
+	for (a = 0; dest[a] != '\0' && a < n; a++)
 	{
 		dest[a] = '\0';
 	}
