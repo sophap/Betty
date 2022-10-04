23343d8424f9c62266171667bd5d07710d064f7f
Right Praise <rightpraise@gmail.com>
2022-10-04 13:13:14 -0700

    Updates code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,5 +39,10 @@ int **alloc_grid(int width, int height)
 		for (n = 0; n < height; n++)
 			ptr[m][n] = 0;
 	}
+	if (ptr[m][n] == NULL)
+	{
+		free(ptr[m][n]);
+		return (NULL);
+	}
 return (ptr);
 }
