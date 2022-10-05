1b3575ce0ee5361945b5cba71f51b447fe708a0c
Right Praise <rightpraise@gmail.com>
2022-10-04 20:33:09 -0700

    Updates code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,7 +31,7 @@ char **strtow(char *str)
 	m = _str_len(str);
 	if (m == 1)
 		printf("Failed\n");
-	ptr = malloc(m * sizeof(char));
+	ptr = malloc(m * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);
 	ptr[m - 1] = NULL;
@@ -43,7 +43,7 @@ char **strtow(char *str)
 			for (o = 1; str[n + o] != ' ' && str[n + o]; o++)
 				;
 			o++;
-			ptr[r] = malloc(o * sizeof(char));
+			ptr[r] = malloc(o * sizeof(**ptr));
 			o--;
 			if (ptr[r] == NULL)
 			{
