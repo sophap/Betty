43d58ab26ca4fbd228d13660c1bcad6d71e227e3
Right Praise <rightpraise@gmail.com>
2022-10-10 13:14:46 -0700

    Defines a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - allows to combine data items of
+ * different kinds
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
