1edd46bf78bb98a34c583860ef166815ff9c8ff0
Right Praise <rightpraise@gmail.com>
2022-10-12 13:52:16 -0700

    Corrects code output

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -9,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
