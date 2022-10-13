25ac5efc5bf1a616c7efaaf669f5d35f4b7a5e86
Right Praise <rightpraise@gmail.com>
2022-10-13 15:55:52 -0700

    Removes whitespace from output

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		if (separator && g < n - 1)
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 	va_end(ap);
 	printf("\n");
