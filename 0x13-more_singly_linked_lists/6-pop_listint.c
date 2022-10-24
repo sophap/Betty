69526c8149e3a3b9ba20e5fbf77d3ecc9e648164
Right Praise <rightpraise@gmail.com>
2022-10-24 15:56:25 -0700

    Corrects code

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,12 +7,13 @@
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *ptr;
 	int str;
 
 	if (*head == NULL)
 		return (0);
 
-	listint_t *ptr = *head;
+	ptr = *head;
 
 	str = (*head)->n;
 	(*head) = (*head)->next;
