4ef5dd7e370c7cf6d97b063073f3a5609bb1a164
Right Praise <rightpraise@gmail.com>
2022-10-24 15:24:55 -0700

    Rearranges code

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,13 +10,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr = malloc(sizeof(listint_t));
 
-	ptr->n = n;
-	ptr->next = NULL;
-
 	if (ptr == NULL)
 		return (NULL);
 
 	ptr->next = *head;
+	ptr->n = n;
 	*head = ptr;
 
 	return (ptr);
