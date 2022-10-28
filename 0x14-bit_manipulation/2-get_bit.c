6470764a8054ac594cc7c22cb4e4d76ce0c60487
Right Praise <rightpraise@gmail.com>
2022-10-27 18:09:06 -0700

    Returns the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to be checked
+ * @index: index to check
+ * Return: the value of a bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int res = 0;
+
+	if (n < 1)
+		return (-1);
+	res = (n >> index) & 1;
+	return (res);
+}
